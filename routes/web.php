0c2de14f2c2e8a3d1bd1caa9d9773d1c3eafdaf2
Fox <christian.graf@cyberfox.at>
2024-10-01 10:13:34 +0200

    enhan: Add calendar controller stub and change it with dashboard.

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
-use Illuminate\Foundation\Application;
-use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Application;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\CalendarController;
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
@@ -14,14 +15,21 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('/dashboard', function () {
+/* Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard'); */
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/calendar', [CalendarController::class, 'index'])->name('calendar');
+    //Route::post('/calendar/event', [ProfileController::class, 'addEvent'])->name('calendar.event.add');
+    //Route::patch('/calendar/event', [ProfileController::class, 'updateEvent'])->name('calendar.event.add');
+    //Route::delete('/calendar/event', [ProfileController::class, 'deleteEvent'])->name('calendar.event.add');
 });
 
 require __DIR__.'/auth.php';
